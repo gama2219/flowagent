af4d70af142a1bf2e6cb900dead37b43dc92a250
gama2219 <169055701+gama2219@users.noreply.github.com>
2025-11-16 13:49:22 +0300

    49-commit

--- frontend/app/api/langgraph/fetch-sessions/route.js
+++ frontend/app/api/langgraph/fetch-sessions/route.js
@@ -1,11 +1,15 @@
 import {Client} from "@langchain/langgraph-sdk"
 import { NextResponse } from "next/server"
+import { createClient } from "@/lib/supabase/server";
 
 
 export async function POST(request){
+    const supabase = await createClient()
+    const { data:{session}} = await supabase.auth.getSession()
+    let auth = session?.access_token
     //meant to fetch sessions
     try{
-    const {n8n_api_key,auth}= await request.json()
+    const {n8n_api_key}= await request.json()
     
     const client = new Client({ apiUrl: process.env.LANGGRAPH_SERVER_URL,defaultHeaders:{Authorization: `Bearer ${auth}`,"X-N8N-API-KEY":n8n_api_key}})
     const sessions=await client.threads.search()
