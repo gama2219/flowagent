ff4f821cdbd194bb75403975e932ad29e94e9825
gama2219 <169055701+gama2219@users.noreply.github.com>
2025-11-16 13:49:52 +0300

    51-commit

--- frontend/hooks/use-messages.js
+++ frontend/hooks/use-messages.js
@@ -10,7 +10,7 @@ export function useMessages(sessionId) {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
   const { user,profile,session} = useAuth()
-  const workflowService = new WorkflowService(profile?.n8n_key,session?.access_token)
+  const workflowService = new WorkflowService(profile?.n8n_key)
 
   useEffect(() => {
     if (sessionId) {
