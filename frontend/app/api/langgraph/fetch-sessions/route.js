b80bc294ed0e20b2b498257ee9e8161a0374ce41
gama2219 <169055701+gama2219@users.noreply.github.com>
2025-11-19 14:20:56 +0300

    67-commit

--- frontend/app/api/langgraph/fetch-sessions/route.js
+++ frontend/app/api/langgraph/fetch-sessions/route.js
@@ -9,9 +9,9 @@ export async function POST(request){
     let auth = session?.access_token
     //meant to fetch sessions
     try{
-    const {n8n_api_key}= await request.json()
+    const {n8n_api_key,n8n_url}= await request.json()
     
-    const client = new Client({ apiUrl: process.env.LANGGRAPH_SERVER_URL,defaultHeaders:{Authorization: `Bearer ${auth}`,"X-N8N-API-KEY":n8n_api_key}})
+    const client = new Client({ apiUrl: process.env.LANGGRAPH_SERVER_URL,defaultHeaders:{Authorization: `Bearer ${auth}`,"X-N8N-API-KEY":n8n_api_key,"X-N8N-ENDPOINT":n8n_url,}})
     const sessions=await client.threads.search()
     
     return NextResponse.json({sessions:sessions})
