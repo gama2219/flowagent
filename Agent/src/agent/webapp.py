6f6cdee8b7a78512995be9181200c6dd6c4cce00
gama2219 <gamamima0@gmail.com>
2025-12-22 17:31:17 +0300

    80

--- Agent/src/agent/webapp.py
+++ Agent/src/agent/webapp.py
@@ -3,63 +3,24 @@ from contextlib import asynccontextmanager
 from src.agent.utils.dbcreation import database_creation
 from requests import request
 from typing import Annotated
+from pathlib import Path
+from v_db import setup_chroma_db
 import os 
 
+vector_path = Path('chroma_db')
+id ='1-wf5LVkvhUZgu4xKfSPcO7zhOBVRAOB6'
+output_path = 'chroma_export.pkl'
 
-app = FastAPI()
-
-
-
-
-
-
-@app.get("/n8n_user")
-def n8n_user(x_n8n_api_key:Annotated[str | None, Header()],n8n_endpoint:Annotated[str | None, Header()])->dict:
-
-    """
-    the endpoint should be a complete endpoint f'{n8n_endpoint}/api/v1/users/{email}?includeRole=true'
-    """
-
-    headers={
-        'accept': 'application/json',
-        'X-N8N-API-KEY':x_n8n_api_key
-    }
-
-    response=request('GET',n8n_endpoint,headers=headers)
-    response.raise_for_status()
-
-    """
-    response 
-    """
+@asynccontextmanager
+async def lifespan(app: FastAPI):
 
 
-    return response.json()
-
-
-@app.get("/workflows")
-def workflows(x_n8n_api_key:Annotated[str | None, Header()],n8n_endpoint:Annotated[str | None, Header()])->dict:
+    if vector_path.exists():
+        print("vector database exists")
+        pass
+    else:
+        setup_chroma_db(output_path,id)
     
-    """
-    the ebdpoint should be a complete endpoint f'{n8n_endpoint}/api/v1/workflows?excludePinnedData=true&limit=100'
-    """
+    yield
 
-    headers={
-        'accept': 'application/json',
-        'X-N8N-API-KEY':x_n8n_api_key
-    }
-    response=request('GET',n8n_endpoint,headers=headers)
-
-    response.raise_for_status()
-    response_body=response.json()
-
-    return_response={'data':[x.get('name') for x in response_body.get('data') if  x.get('isArchived')==False]}
-    """
-    return dictionary  {data:[..list of workflows name]}
-    """
-    
-    return_response
-
-@app.get("/test")
-def test_endpoint(n8n_endpoint:Annotated[str | None, Header()]):
-
-    return {'headers':n8n_endpoint}
+app = FastAPI(lifespan=lifespan)
